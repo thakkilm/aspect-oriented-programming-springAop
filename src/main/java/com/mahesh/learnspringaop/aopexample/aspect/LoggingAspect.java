33f16d5ed9d534f913277c1c7f4d524d5eb9729c
Mahesh <thakkilm@mail.gvsu.edu>
2023-11-02 15:01:16 -0400

    User @After annotaion to show the functionality of this aspect annotation

--- src/main/java/com/mahesh/learnspringaop/aopexample/aspect/LoggingAspect.java
+++ src/main/java/com/mahesh/learnspringaop/aopexample/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.mahesh.learnspringaop.aopexample.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -14,11 +15,16 @@ public class LoggingAspect {
 	Logger logger=LoggerFactory.getLogger(getClass());
 	
 	
-	
+//	Executes before method is called
 	@Before("execution(* com.mahesh.learnspringaop.*.*.*.*(..))")
-	public void logMethodCall(JoinPoint joinPoint) {
+	public void logMethodCallBeforeExecution(JoinPoint joinPoint) {
 		logger.info("Before Aspect - Method called is {}",joinPoint);
 	}
 	
+//	Executed after the method is successfully executed or throwed any exception
+	@After("execution(* com.mahesh.learnspringaop.*.*.*.*(..))")
+	public void logMethodCallAfterExecution(JoinPoint joinPoint) {
+		logger.info("After Aspect -{} method argumenmts {}", joinPoint,joinPoint.getArgs());
+	}
 	
 }
